e1f1d0271e29d08d580c932f1503ba76a7b6bf0f
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-09-01 17:18:36 -0400

    y

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const array = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+    return function(){
+        return integer * 4
+    }
+}
+
+const fareDoubler = function(integer){
+    return integer * 2
+}
